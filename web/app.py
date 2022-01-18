f524f3e18c28fbc983bb6a0895aa363de613c312
alex.fayers <alex@fayers.com>
2022-01-18 00:14:04 +0000

    predictions!

--- web/app.py
+++ web/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import render_template
 
+from calculations import calc_average_minutes
+from datetime import datetime, timedelta
+
 app = Flask(__name__)
 
 
@@ -12,17 +15,42 @@ def status():
     timestamp = status.split(', ')[0]
     status = status.split(', ')[1]
 
+    timestamp = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
+    
+    average_wait = calc_average_minutes('../status_log')        
+
     if status == "open":
         status = "No"
         color = "status-0"
         subtext = "You can probably use it"
+        wait_message = f"Average in-use time: {average_wait} minutes"
     elif status == "closed":
+        previous_prediction = None
+        future_timestamp = timestamp + timedelta(minutes=average_wait)
+        wait_info = ""
+
+        while datetime.now() > future_timestamp:
+            wait_info = f" (for usages longer than {average_wait} minutes)"
+            average_wait = calc_average_minutes('../status_log', min_diff=60*average_wait)
+            future_timestamp = timestamp + timedelta(minutes=average_wait)
+
+            if future_timestamp == previous_prediction:
+                break
+            else:
+                previous_prediction = future_timestamp
+
         status = "Yes"
         color = "status-4"
         subtext = "You can try to use it but it might be awkward"
+
+        wait_message = f"Predicted wait time: {average_wait} minutes{wait_info}"
+        wait_message += f" (probably free at like <b>{future_timestamp.strftime('%H:%M')}</b>)"
+
+    minutes_ago = f" - {int((datetime.now() - timestamp).total_seconds() // 60)} minutes ago"
+
     
-    return render_template('index.jinja.html', status=status, color=color, subtext=subtext, timestamp=timestamp)
+    return render_template('index.jinja.html', status=status, color=color, subtext=subtext, timestamp=timestamp.strftime('%H:%M'), wait_message=wait_message, minutes_ago=minutes_ago)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False)
+    app.run(host='0.0.0.0', debug=True)
---
+++ web/calculations.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+def calc_average_minutes(log_filename, min_diff = 60):
+    total_diff = 0
+    total_n = 0
+
+    previous_status = ''
+    previous_timestamp = None
+
+    with open(log_filename, 'r') as status_log:
+        for line in status_log:
+            
+            timestamp, status = line.split(', ')
+            status = status.strip()
+
+            if status == previous_status:
+                continue
+
+            timestamp = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
+
+            if previous_timestamp is not None:
+                diff = (timestamp - previous_timestamp).total_seconds()
+                minute_diff = f"{int(diff // 60)} minutes"
+                
+                if status == 'open':
+                    if diff > min_diff and diff < 60 * 60 * 6:
+                        total_n += 1
+                        total_diff += diff
+                        # print(f"Time in toilet at {timestamp}: {minute_diff}")
+                
+            previous_status = status
+            previous_timestamp = timestamp
+
+    if total_n == 0:
+        return 999
+    else:
+        return int(total_diff / total_n / 60)
